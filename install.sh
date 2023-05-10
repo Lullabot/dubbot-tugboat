0edb26c32afc8f8baad0f41f00a7acb4521e1fa4
Pablo López <pablo.lopez@lullabot.com>
2023-05-10 17:04:40 +0200

    Add install.sh script

---
+++ install.sh
@@ -0,0 +1,9 @@
+drush site-install --yes
+drush user:password admin admin
+drush user:role:add administrator admin
+drush config-set system.site uuid 28350631-bb36-48a0-a913-6d4998c3c9db --yes
+drush entity-delete shortcut_set --yes
+drush config-import --yes
+
+# For restoring shortcuts.
+drush php-eval 'include_once "core/profiles/standard/standard.install"; standard_install();'
